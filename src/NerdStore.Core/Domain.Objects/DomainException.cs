721c06f7b687fc853c24a53385289b8e49519fca
Rafael Barbosa <rafha4061@gmail.com>
2025-11-25 19:11:07 -0300

    Validações de entidades

---
+++ src/NerdStore.Core/Domain.Objects/DomainException.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace NerdStore.Core.Domain.Objects
+{
+    public class DomainException : Exception
+    {
+        public DomainException()
+        {
+            
+        }
+
+        public DomainException(string message) : base(message)
+        {
+            
+        }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        }
+    }
+}
--- src/NerdStore.Core/obj/Debug/net8.0/NerdStore.Core.AssemblyInfo.cs
+++ src/NerdStore.Core/obj/Debug/net8.0/NerdStore.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NerdStore.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f522390c78633caed9186d83e658fce74ce54ae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NerdStore.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NerdStore.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
