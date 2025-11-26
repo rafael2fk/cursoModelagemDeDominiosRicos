597cb72ef2736db1b4571cfd678f84f4df8d59d6
Rafael Barbosa <rafha4061@gmail.com>
2025-11-26 13:08:07 -0300

    Lançando eventos de domínio

--- src/NerdStore.Catalogo.Domain/obj/Debug/net8.0/NerdStore.Catalogo.Domain.AssemblyInfo.cs
+++ src/NerdStore.Catalogo.Domain/obj/Debug/net8.0/NerdStore.Catalogo.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NerdStore.Catalogo.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72a4a34498d2a42437dbc40bbe1d52645929b84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df409e63aa9d8a563ddf5970dd375874f37ee2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NerdStore.Catalogo.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NerdStore.Catalogo.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/NerdStore.Core/Domain.Objects/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.Domain.Objects
+{
+    public class DomainEvent : Event
+    {
+        public DomainEvent(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
